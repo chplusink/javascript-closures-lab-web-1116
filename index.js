244ed73485a0c10dbd9185fada3a3f8381fa3cc0
Vic Lam <lam.vic.k@gmail.com>
2017-01-24 15:58:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    var counter = 0
+
+    function addBump() {
+        counter++
+    }
+
+    function getBumps() {
+        return counter
+    }
+
+    return {
+        addBump, getBumps
+    }
+}
+
+function createAnimal(animalType) {
+
+    function combineAnimalAndDevice(deadlyDevice) {
+        return {
+            animalType: animalType,
+            deadlyDevice: deadlyDevice
+        }
+    }
+
+    return combineAnimalAndDevice
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
